8c6984219f6841be89692e45c369b804e6b5f5c8
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2021-01-31 11:20:03 -0500

    Update and rename arrow_clover.mk to havoc_clover.mk

--- havoc_clover.mk
+++ havoc_clover.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Arrow stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Inherit from clover device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -29,7 +29,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := clover
 PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_clover
+PRODUCT_NAME := havoc_clover
 PRODUCT_MODEL := MI PAD 4
 
 DEVICE_MAINTAINER := TH779
